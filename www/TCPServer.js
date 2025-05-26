a5d4ea669ac8a771a36059cd5e02e97181451557
MVREDDY-STI <61783693+MVREDDY-STI@users.noreply.github.com>
2025-05-26 14:47:39 +0900

    Update TCPServer.js

--- www/TCPServer.js
+++ www/TCPServer.js
@@ -8,6 +8,10 @@ exports.startServer = function (arg0, success, error) {
     exec(success, error, 'TCPServer', 'startServer', [arg0]);
 };
 
+exports.restartServer = function (arg0, success, error) {
+    exec(success, error, 'TCPServer', 'restartServer', [arg0]);
+};
+
 exports.stopServer = function (success, error) {
     exec(success, error, 'TCPServer', 'stopServer', []);
 };
